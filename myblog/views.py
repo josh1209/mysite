79ff89cc61f03fd7afa87c0a3f8e758bb92fa05d
Josh Hoff <jhoff1985@hotmail.com>
2019-02-10 19:53:11 -0800

    Added detail_view

--- myblog/views.py
+++ myblog/views.py
@@ -19,5 +19,13 @@ def list_view(request):
     posts = published.order_by('-published_date')
     template = loader.get_template('list.html')
     context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type='text/html')
+    return render(request, 'list.html', context)
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'detail.html', context)

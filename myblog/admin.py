5348dea97cc14ab4949dae179374b6c1dac93c83
Josh Hoff <jhoff1985@hotmail.com>
2019-02-11 21:31:00 -0800

    Finished adjusting django blog to match assignment requests

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from myblog.models import Post
-from myblog.models import Category
+from myblog.models import Post, Category, PostAdmin, CategoryAdmin
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
--- myblog/models.py
+++ myblog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
 
 # Create your models here.
 
@@ -10,10 +11,11 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
-    
+
+
     def __str__(self):
         return self.title
-    
+
 class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
@@ -21,6 +23,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name_plural = 'Categories'
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+    #this line is probably redundant, as I haven't included it in my list display.
+    exclude = ['posts']
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+    list_display = ['title', 'text', 'author']
